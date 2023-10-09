d7ea4936256f59ce1920ae27e32ac75798cf3eb4
wilsonandhisgreenbike <quanganhha99@gmail.com>
2023-10-09 14:14:58 +0700

    fix responsive

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -5,6 +5,7 @@ function MusicPlayer() {
       style={{ borderRadius: "12px" }}
       src="https://open.spotify.com/embed/playlist/37i9dQZF1DWZeKCadgRdKQ?utm_source=generator&theme=0"
       width="100%"
+      height="352"
       frameBorder="0"
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       loading="lazy"
